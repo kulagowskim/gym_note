dcb0723f43404c3706f49be0e8f3540d4bcec8cd
kulagowskim <matkulagowski@gmail.com>
2020-06-30 01:22:06 +0200

    Added backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
+
+  const callAPI = () => {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => setApiResponse(res));
+  }
+
+  useEffect(() => callAPI(), [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{apiResponse}</p>
       </header>
     </div>
   );
